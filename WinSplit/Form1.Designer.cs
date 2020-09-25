17c9b94bd038878464970e7c50eb84dfb1637ab1
tkonuma <toshiharu@SUR-PAX-0004.yjoffice.local>
2020-09-25 21:34:18 +0900

    created a tool to split file.

---
+++ WinSplit/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WinSplit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.txtLine = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnSplit.Location = new System.Drawing.Point(16, 16);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(492, 76);
+            this.btnSplit.TabIndex = 1;
+            this.btnSplit.Text = "ファイルを選択して分割開始";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // txtLine
+            // 
+            this.txtLine.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtLine.Location = new System.Drawing.Point(761, 26);
+            this.txtLine.Name = "txtLine";
+            this.txtLine.Size = new System.Drawing.Size(207, 50);
+            this.txtLine.TabIndex = 2;
+            this.txtLine.Text = "200000";
+            this.txtLine.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(974, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 43);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "行ずつ分割";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtStatus.Location = new System.Drawing.Point(12, 113);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(1700, 504);
+            this.txtStatus.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(567, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 43);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ファイルを";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1724, 629);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLine);
+            this.Controls.Add(this.btnSplit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ファイル分割";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.TextBox txtLine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WinSplit/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSplit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+			System.Diagnostics.Debug.WriteLine("---> START");
+			txtStatus.Clear();
+
+			string filePath = selectFile();
+			System.Diagnostics.Debug.WriteLine("- File = \"" + filePath + "\"");
+			if (filePath == "")
+			{
+				txtStatus.Text = "失敗：ファイルが選択されていません";
+				return;
+			}
+			string fileDir = System.IO.Path.GetDirectoryName(filePath);
+			string fileBase = System.IO.Path.GetFileNameWithoutExtension(filePath);
+			string fileExt = System.IO.Path.GetExtension(filePath);
+
+			int unitLine;
+			if (int.TryParse(txtLine.Text, out unitLine) == false)
+			{
+				txtStatus.Text = "失敗：行数に指定の [" + txtLine.Text + "] は数値ではありません";
+				return;
+			}
+
+			txtStatus.Text += $"■以下ファイルを {unitLine:#,0} 行単位で分割します\r\n";
+			txtStatus.Text += $"{filePath}\r\n\r\n";
+
+			txtStatus.Text += "≫ 分割開始 ≫≫≫\r\n";
+			txtStatus.Refresh();
+
+			int b;
+			FileStream infs = new FileStream(filePath, FileMode.Open);
+			FileStream outfs = null;
+
+			int countLf = 0;
+			int countLine = 0;
+			int newFileCount = 0;
+			string newPath = "";
+			while ((b = infs.ReadByte()) != -1)
+			{
+				if (outfs == null)
+				{
+					newPath = $"{fileDir}\\{fileBase}_{++newFileCount:000}{fileExt}";
+					outfs = new FileStream(newPath, FileMode.Create);
+					txtStatus.Refresh();
+				}
+				outfs.WriteByte((byte)b);
+				if (b == 10)
+				{
+					countLf++;
+					countLine++;
+					if ((countLine % unitLine) == 0)
+					{
+						countLine = closeNewFile(ref outfs, newPath, countLine);
+					}
+				}
+			}
+			infs.Close();
+			if (outfs != null)
+            {
+//				outfs.Close();
+				countLine = closeNewFile(ref outfs, newPath, countLine);
+			}
+
+			txtStatus.Text += "≫ 分割終了 ≫≫≫\r\n\r\n";
+			txtStatus.Text += $"全 {countLf:#,0} 行を処理しました";
+
+			System.Diagnostics.Debug.WriteLine("<--- FINISH");
+		}
+
+		private String selectFile()
+		{
+			String filePath = "";
+			OpenFileDialog ofd = new OpenFileDialog();
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				filePath = ofd.FileName;
+
+			}
+			return filePath;
+		}
+
+		private int closeNewFile(ref FileStream outfs, string newPath, int countLine)
+        {
+			outfs.Close();
+			outfs = null;
+			txtStatus.Text += $"{newPath}\t{countLine:#,0}行\r\n";
+			countLine = 0;
+
+			return countLine;
+		}
+
+	}
+}
